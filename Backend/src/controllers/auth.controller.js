f6873fd2a8e058e7036be78def3a0575c0fb595e
vanurag02 <anvaidya6@gmail.com>
2026-05-12 07:42:55 +0530

    feat: Register and Login Controller | May 12, 2026

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -1,16 +1,19 @@
-/* =============== USER MODEL IMPORT =============== */
+/* =============== IMPORTS =============== */
 const userModel = require("../models/user.model.js");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
+/* =============== REGISTER USER CONTROLLER =============== */
 /**
 @name - registerUserController
 @description REGISTER A NEW USER, EXPECTS USERNAME, EMAIL, AND PASSWORD IN THE BODY
-@access - PUBLIC
+@access PUBLIC
 */
 
 async function registerUserController(req, res) {
   const { username, email, password } = req.body;
 
-  // CHECKING THE INPUTS
+  // CHECKING THE INPUT
   if (!userName || !email || !password) {
     return res.status(400).json({
       message: "Please provide username, email, and password.",
@@ -22,12 +25,108 @@ async function registerUserController(req, res) {
     $or: [{ username }, { email }],
   });
 
+  // CHECKING IF ALREADY EXIST WITH USERNAME AND EMAIL
   if (isUserAlreadyExist) {
+    // USER ALREADY EXIST WITH USERNAME
+    if (isUserAlreadyExist.username === username) {
+      return res.status(400).json({
+        message: "Account with this username already exist.",
+      });
+    }
+
+    // USER ALREADY EXIST WITH EMAIL
+    if (isUserAlreadyExist.email === email) {
+      return res.status(400).json({
+        message: "Account with this email already exist.",
+      });
+    }
+  }
+
+  /* =============== HASHING THE PASSWORD =============== */
+  const hash = await bcrypt.hash(password, 10);
+
+  /* =============== CREATING A USER =============== */
+  const user = await userModel.create({
+    username,
+    email,
+    password: hash,
+  });
+
+  /* =============== CREATING TOKEN =============== */
+  const token = jwt.sign(
+    {
+      id: user._id,
+      username: user.username,
+    },
+    process.env.JWT_SECRET,
+    { expiresIn: "1d" },
+  );
+
+  /* =============== SETTING ABOVE TOKEN INTO COOKIE =============== */
+  res.cookie("token", token);
+
+  res.status(201).json({
+    message: "User registered successfully.",
+    user: {
+      id: user._id,
+      username: user.username,
+      email: user.email,
+    },
+  });
+}
+
+/* =============== LOGIN USER CONTROL =============== */
+/**
+@name - loginUserController
+@description LOGIN A NEW USER, EXPECTS EMAIL, AND PASSWORD IN THE BODY
+@access - PUBLIC
+*/
+
+async function loginUserController(req, res) {
+  const { email, password } = req.body;
+
+  // CHECK IF ANY USER EXIST WITH THIS EMAIL
+  const user = await userModel.findOne({ email });
+
+  if (!user) {
     return res.status(400).json({
-      message: "Account already exist with this username or email",
+      message: "Invalid email or password.",
     });
   }
+
+  // CHECKING PASSWORD
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+
+  if (!isPasswordValid) {
+    return res.status(400).json({
+      message: "Invalid email or password.",
+    });
+  }
+
+  /* =============== CREATING TOKEN =============== */
+  const token = jwt.sign(
+    {
+      id: user._id,
+      username: user.username,
+    },
+    process.env.JWT_SECRET,
+    { expiresIn: "1d" },
+  );
+
+  /* =============== SETTING ABOVE TOKEN INTO COOKIE =============== */
+  res.cookie("token", token);
+
+  res.status(201).json({
+    message: "User logged in successfully.",
+    user: {
+      id: user._id,
+      username: user.username,
+      email: user.email,
+    },
+  });
 }
+
 module.exports = {
   registerUserController,
+  loginUserController,
 };
--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -2,14 +2,23 @@
 const express = require("express");
 const authRouter = express.Router();
 
-/* =============== EXPRESS IMPORT =============== */
+/* =============== AUTH CONTROLLER =============== */
 const authController = require("../controllers/auth.controller.js");
 /**
-@route - POST /api/auth/register
-@description - REGISTER A NEW USER
-@access - PUBLIC
-*/
+ @route POST /api/auth/register
+ @description REGISTER A NEW USER
+ @access PUBLIC
+ */
 
 authRouter.post("/register", authController.registerUserController);
+
+/* =============== LOGIN CONTROLLER =============== */
+/**
+@route POST /api/auth/login
+@description LOGS IN USER WITH EMAIL AND PASSWORD
+@access PUBLIC
+*/
+
+authRouter.post("/api/login", authController.loginUserController);
 
 module.exports = authRouter;
