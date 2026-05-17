88641fc158b8f91342d92998d6c73e4ce9204206
vanurag02 <anvaidya6@gmail.com>
2026-05-17 11:20:46 +0530

    Service layer setup & Context state management | May 17, 2026

--- Frontend/src/app.routes.jsx
+++ Frontend/src/app.routes.jsx
@@ -7,6 +7,10 @@ import Register from "./features/auth/pages/Register";
 
 export const router = createBrowserRouter([
   {
+    path: "/",
+    element: <h1>PrepPilot Home</h1>,
+  },
+  {
     path: "/login",
     element: <Login />,
   },
